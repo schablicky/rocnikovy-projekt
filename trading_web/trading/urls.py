ca8e782c83d6cdd44e339afc4b61793fd675d3cb
matejzimmer <maty.zimmer@gmail.com>
2024-12-08 18:54:19 +0100

    web

--- trading_web/trading/urls.py
+++ trading_web/trading/urls.py
@@ -5,5 +5,4 @@ app_name = 'trading'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
 ]
--- trading_web/trading/views.py
+++ trading_web/trading/views.py
@@ -1,5 +1,13 @@
+# trading/views.py
 from django.shortcuts import render
+from .models import TradingPair, Trade, AIModel
 
-# Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    context = {
+        'trading_pairs': TradingPair.objects.filter(is_active=True)[:5],
+        'recent_trades': Trade.objects.select_related('pair').order_by('-opened_at')[:5],
+        'ai_models': AIModel.objects.select_related('pair').all()[:3],
+        'total_trades': Trade.objects.count(),
+        'successful_trades': Trade.objects.filter(profit_loss__gt=0).count()
+    }
+    return render(request, 'home.html', context)
