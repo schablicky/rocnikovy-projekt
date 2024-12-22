31f62307cf4dd57e00685000d83059ea7cb3bf3a
schablicky <tomasschablicky@gmail.com>
2024-12-22 17:25:30 +0100

    spojen djangoUser a customUser

--- trading_web/trading/admin.py
+++ trading_web/trading/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import *
 
-admin.site.register([User, Trade, News, 
+admin.site.register([CustomUser, Trade, News, 
                     CopyTrader, Message, MarketData])
 
